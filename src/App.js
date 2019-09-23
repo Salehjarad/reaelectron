46b0526a9a600467718fa208b71bb18704802221
Salehjarad <salehjarad.art@gmail.com>
2019-09-23 19:24:34 +0300

    adding electron

--- src/App.js
+++ src/App.js
@@ -2,25 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const ipc = window.ipcRenderer;
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    }
+  }
+  componentWillMount(){
+    ipc.send('user');
+  }
+
+  componentDidMount(){
+    ipc.on('userinfo', (event, data) => {
+      this.setState({ username: data.username })
+    })
+  
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React {this.state.username}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
